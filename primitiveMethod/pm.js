6233a4734b323b3cd55d5ccab1ec3732313c9afb
배재익 <0308bae@naver.com>
2022-01-21 15:22:28 +0900

    '.'

---
+++ primitiveMethod/pm.js
@@ -0,0 +1,14 @@
+let john = {
+    name: "John",
+    sayHi: function () {
+        alert("친구야 반갑다!");
+    }
+};
+
+john.sayHi(); // 친구야 반갑다
+
+let str = "Hello";
+alert(str.toUpperCase()); // HELLO
+
+let n = 1.23456;
+alert(n.toFixed(2)); // 1.23
